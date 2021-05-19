ac8c71586f145548da2e80e5a275a0fffaa9ba89
Roberto Luís Rezende <robertodermato@gmail.com>
2021-05-19 00:17:33 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name, name2):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Oi, {name} e {name2}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Roberto', 'Cris')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
